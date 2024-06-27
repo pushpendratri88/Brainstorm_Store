858c593f8ec9fc7439fe75b6ddbda21628745d9d
Pushpendra.tripathi <pushpendra.tri88@gmail.com>
2024-06-27 14:13:12 +0530

    Registered all microservice with Eureka server

--- order/src/main/java/com/brainstorm/order/OrderApplication.java
+++ order/src/main/java/com/brainstorm/order/OrderApplication.java
@@ -2,10 +2,10 @@ package com.brainstorm.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@ComponentScan("com.brainstorm.order")
+@EnableDiscoveryClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
