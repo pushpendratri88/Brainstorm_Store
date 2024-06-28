ddd538d6f11464041d79dd05dc6c6b0268b6c1cd
Pushpendra.tripathi <pushpendra.tri88@gmail.com>
2024-06-28 13:08:36 +0530

    Implemented Circuitbreaker for cascade failuer

--- order/src/main/java/com/brainstorm/order/service/impl/CustomerServiceImpl.java
+++ order/src/main/java/com/brainstorm/order/service/impl/CustomerServiceImpl.java
@@ -3,20 +3,59 @@ package com.brainstorm.order.service.impl;
 import com.brainstorm.order.dto.CustomerDTO;
 import com.brainstorm.order.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
+
     @Autowired
     private final RestTemplate restTemplate;
+
+
+    @Autowired
+    CircuitBreakerFactory circuitBreakerFactory;
+
+    @Value("${customer.service.url}")
+    private String customerServiceUrl;
 
     public CustomerServiceImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
+    @Override
     public CustomerDTO getCustomer(String customerId) {
-        String url = "http://localhost:8080/api/fetchCustomerDetails?input=" + customerId;
-        return restTemplate.getForObject(url, CustomerDTO.class);
+        String url = customerServiceUrl + "/fetchCustomerDetails?input=" + customerId;
+        logger.info("Requesting customer details from URL: ", url);
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("customerService");
+        return circuitBreaker.run(() -> {
+            try{
+                return restTemplate.getForObject(url, CustomerDTO.class);
+            }
+            catch (Exception e){
+                logger.error("Error while requesting customer details", e);
+                throw e;
+            }
+        }, throwable -> {
+            logger.error("Customer service is down, returning fallback response", throwable);
+            return getDefaultCustomer();
+        });
+    }
+
+    private CustomerDTO getDefaultCustomer() {
+        CustomerDTO fallbackCustomer = new CustomerDTO();
+        fallbackCustomer.setId("Cust_Default");
+        fallbackCustomer.setName("Default Customer");
+        fallbackCustomer.setEmail("default@example.com");
+        return fallbackCustomer;
     }
 }
